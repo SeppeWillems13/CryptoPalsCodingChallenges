f82055cd93821045b2e9f1443e9a3bf2259ca7a4
SeppeWillems13 <willemss14@gmail.com>
2023-02-16 11:37:25 +0100

    Add set 1

---
+++ ConsoleApp1/HexToBase64.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1;
+
+public static class HexToBase64
+{
+    public static string ConvertToHex(string hex)
+    {
+        var bytes = Enumerable.Range(0, hex.Length)
+            .Where(x => x % 2 == 0)
+            .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+            .ToArray();
+
+        return Convert.ToBase64String(bytes);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1;
+
+const string hexString = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+const string expectedOutcome = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+var result = HexToBase64.ConvertToHex(hexString);
+Console.WriteLine(result == expectedOutcome ? "Success!" : "Failure!");
